6100d112758f4e4a3c353a1e946b99dccdeb8bc2
Saulius Baltramonaitis <msnfan234@gmail.com>
2021-05-08 15:33:23 +0100

    implement connection to database

--- database.js
+++ database.js
@@ -19,7 +19,7 @@ function connectToDB() {
 function getTime() {
     let time = null;
     client.connect();
-    client.query('SELECT NOW()', (err, res) => {
+    client.query('SELECT NOW();', (err, res) => {
         if (err) { console.log(err.stack) }
         else {
             time = res.rows[0];
@@ -28,6 +28,18 @@ function getTime() {
         }
     })
     return time;
+}
+
+function getTestData() {
+    let output = null;
+    client.query('SELECT * FROM SRD;', (err, res) => {
+        if (err) { console.log(err.stack) }
+        else {
+            output = res.rows[0];
+            console.log(output);
+            client.end();
+        }
+    })
 }
 
 module.exports = {
