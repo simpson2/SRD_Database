e12feac4f9a05447da1fa9d59f88a1629d15187b
Saulius Baltramonaitis <msnfan234@gmail.com>
2021-05-08 13:53:52 +0100

    implement connection to database

--- queries.js
+++ queries.js
@@ -1,4 +1,4 @@
-const time = require("./database")
+const DB = require("./database")
 
 const getHome = (req, res) => {
     return res.json({ Name: 'SRD_Database', Info: 'Node.JS, Express', Server: 'Online'});
