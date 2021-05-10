2d5cb2369f26f5e3e942514f1084b7bfea099a35
Saulius Baltramonaitis <msnfan234@gmail.com>
2021-05-10 11:52:53 +0100

    implement service to fetch srd

--- services/printSRD.js
+++ services/printSRD.js
@@ -1,21 +1,27 @@
-if(typeof require !== 'undefined') XLSX = require('xlsx');
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const xlsx = require('xlsx');
+const getSRd = require('./getSRD');
+// const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-const url = 'http://www.nats-uk.ead-it.com/aip/current/srd/SRD_Spreadsheet.xls';
+// const url = 'http://www.nats-uk.ead-it.com/aip/current/srd/SRD_Spreadsheet.xls';
+
+// function printSRD() {
+//     var req = new XMLHttpRequest();
+//     req.open('GET', url, true);
+//     req.responseType = 'arraybuffer';
+
+//     req.onload = (e) => {
+//         var data = new Uint8Array(req.response);
+//         console.log("Response array length:" + data.length);
+//         var workbook = XLSX.read(data, {type: 'array'});
+//         var routes = workbook.Sheets['Routes'];
+//     }
+
+//     req.send();
+// }
 
 function printSRD() {
-    var req = new XMLHttpRequest();
-    req.open('GET', url, true);
-    req.responseType = 'arraybuffer';
-
-    req.onload = (e) => {
-        var data = new Uint8Array(req.response);
-        console.log("Response array length:" + data.length);
-        var workbook = XLSX.read(data, {type: 'array'});
-        var routes = workbook.Sheets['Routes'];
-    }
-
-    req.send();
+    var workbook = xlsx.readFile('SRD.xls');
+    console.log(workbook.SheetNames);
 }
 
 module.exports = printSRD;
